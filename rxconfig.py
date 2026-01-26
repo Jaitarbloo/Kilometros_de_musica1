bd084c81984260e4bd131ae55fbe1c305acd3461
Jaitarbloo <jaitarbloo@yahoo.es>
2026-01-26 20:29:25 +0100

    AÃ±adiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -5,5 +5,12 @@ config = rx.Config(
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
+        api_url="https://kilometros-de-musica1.onrender.com",
+    
+                   			cors_allowed_origins=[ "http://localhost:3000",
+        
+                                          "https://www.kilometrosdemusica.com",
+        
+                                        			]
     ]
 )
